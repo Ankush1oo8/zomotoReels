01596a7cbe2475bf46e8f5ee176d61a93779f4c6
Ankush1oo8 <ankushchudiwal@outlook.com>
2025-09-03 19:41:07 +0530

    basic setup

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+//start server
+const app=require("./src/app");
+
+
+app.listen(3000,()=>{
+    console.log("server running on 3000")
+})
---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+//create server
+const express=require("express");
+
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send("Hello there!!");
+})
+
+
+
+
+
+modules.export=app;
